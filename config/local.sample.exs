fafd130f93d06de877de31abee099b513bbc4dda
Andy Kanga <andy.kanga@heig-vd.ch>
2022-01-04 21:37:21 +0100

    Test changement

--- config/local.sample.exs
+++ config/local.sample.exs
@@ -13,18 +13,18 @@ config :minesweeper, Minesweeper.Repo,
     if(config_env() != :test,
       # This is the production database URL. You can override it at runtime with
       # the $MINESWEEPER_DATABASE_URL environment variable.
-      do: "ecto://minesweeper:changeme@localhost/minesweeper",
+      do: "ecto://minesweeper:Archidep2022@localhost/minesweeper",
       # This is a separate test database URL to avoid overwriting
       # development/production data during tests. You only need to configure
       # this if you want to run the automated tests.
-      else: "ecto://minesweeper:changeme@localhost/minesweeper-test"
+      else: "ecto://minesweeper:Archidep2022@localhost/minesweeper-test"
     )
 
 # Configure the web endpoint.
 config :minesweeper, MinesweeperWeb.Endpoint,
   http: [
     # Set the HTTP port the server listens on.
-    port: 3000
+    port: 3001
   ],
   # The secret key base is used as a base to generate secrets for encrypting and
   # signing data. For example, cookies and tokens are signed by default. This
@@ -35,10 +35,10 @@ config :minesweeper, MinesweeperWeb.Endpoint,
   #
   # Run "mix phx.gen.secret" at the root of this repository to generate a
   # suitable value for the secret key base.
-  secret_key_base: "changeme",
+  secret_key_base: "/wggIPhlgQitXDKd65nnn3XDsd2VoLUR6BdNxQq7MaImMn33edo6eJAvRi9oiIDu",
   # The base URL at which the application is publicly available. You would
   # typically use the same host as in the configuration of your reverse proxy.
   url: [
-    host: "example.com",
+    host: "localhost",
     port: 443
   ]
